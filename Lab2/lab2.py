8642752818efc73406a3af721df5f956c2203e42
Dadmon <maksklym-ipt27@lll.kpi.ua>
2023-11-07 21:23:04 +0200

    ZSUv

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PIL import Image
 import os
-
+from kernels import *
 
 def rgb_to_halftone(img):
     r, g, b = img[:,:,0], img[:,:,1], img[:,:,2]
@@ -13,38 +13,59 @@ def rgb_to_halftone(img):
 
 photo = Image.open("Photos\\test.jpg")
 
-ph_filter = np.array([
-    [-2, -1, 0,],
-    [-1, 0, 1,],
-    [0, 1, 2,],
-], dtype=np.float32)# /7
+ph_filter = ZSUv()# /7
+
+print(ph_filter)
 
 working_ar = np.array(photo)
 working_ar_l = rgb_to_halftone(working_ar).astype(np.uint8)
+
 print(working_ar.shape)
-print(working_ar_l.shape)
+#print(working_ar_l.shape)
 
 filtered_ar = np.zeros_like(working_ar)
 
 ar_size = len(working_ar_l)
 
-height, width = working_ar_l.shape
-height_fl, width_fl = ph_filter.shape
+if type(ph_filter) == np.ndarray:
+    height, width, channels = working_ar.shape
+    height_fl, width_fl = ph_filter.shape
+    
+    height_limit = height_fl - 2
+    width_limit = width_fl - 2
+    
+    for channel in range(channels):
+        for row in range(height_limit, height - height_limit):
+            for col in range(height_limit, width - height_limit):
+                matrix = working_ar[row - height_limit:row + 2, col - width_limit:col + 2, channel]
+                filtered_ar[row, col, channel] = np.sum(matrix * ph_filter)# %256
+    
+    
+    
+    
+    filtered_ar = np.clip(filtered_ar, 0, 255).astype(np.uint8)
+    
+elif type(ph_filter) == list:
+    [x_shift, y_shift] = ph_filter
+    filtered_ar[y_shift:, x_shift:] = working_ar[:-y_shift, :-x_shift]
+    
+photo = Image.fromarray(filtered_ar)
+
+try:
+    os.mkdir('Results\\')
+except FileExistsError:
+    pass
+
+photo.save('Results\\test_result.jpg')
+
+"""
+Trash
 # print(working_ar[..., 2])
 
 # for i in range (0, 3):
 #     for j in range(0, 3):
 #         print(i, j, ph_filter[i][j])
 #         # print(ph_filter[i][j])
-
-height_limit = height_fl - 2
-width_limit = width_fl - 2
-
-# for channel in range(channels):
-for row in range(height_limit, height - height_limit):
-    for col in range(height_limit, width - height_limit):
-        matrix = working_ar_l[row - height_limit:row + 2, col - width_limit:col + 2]
-        filtered_ar[row, col] = np.sum(matrix * ph_filter)# %256
 
 
     #         sum_by_filter = 0
@@ -61,14 +82,4 @@ for row in range(height_limit, height - height_limit):
 
 # print(working_ar_l)
 # print(filtered_ar)
-
-filtered_ar = np.clip(filtered_ar, 0, 255).astype(np.uint8)
-
-photo = Image.fromarray(filtered_ar)
-
-try:
-    os.mkdir('Results\\')
-except FileExistsError:
-    pass
-
-photo.save('Results\\test_result.jpg')
+"""
