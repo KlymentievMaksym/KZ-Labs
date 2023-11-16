f7a2d0d414a188d275cd35374da4fb08206ee0e8
Dadmon <maksklym-ipt27@lll.kpi.ua>
2023-11-16 19:01:02 +0200

    Update lab3.py

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -5,6 +5,7 @@ import datetime as dtime
 
 
 def fit(ar3x3):
+    print(ar3x3, fl, np.all(ar3x3 == fl))
     if np.all(ar3x3 == fl): return True
     return False
 
@@ -14,9 +15,10 @@ def hit(ar3x3):
     return False
 
 
-def do_morph(photo, fl, fl_type):
+def do_morph(photo, fl, fl_type, wild_point_loc):
     
     ar_to_return = np.zeros_like(photo)
+    ar_to_return[...] = 255 
     
     height, width, channels = photo.shape
     height_fl, width_fl = fl.shape
@@ -40,10 +42,10 @@ def do_morph(photo, fl, fl_type):
                 row_finish = row + 2
                 
                 for col in range(width_limit, width - width_limit):
-                    if fit(photo[row_start:row_finish, col - width_limit:col + 2, channel]): ar_to_return[row, col, channel] = 255
+                    if fit(photo[row_start:row_finish, col - width_limit:col + 2, channel]): ar_to_return[row + wild_point_loc[0]-1, col + wild_point_loc[1]-1, channel] = 255
                     else: ar_to_return[row, col, channel] = 0
                         
-    elif fl_type == 'hit':
+    elif fl_type == 'fit':
         print('HIT start')
         for channel in range(channels):
             for row in range(height_limit, height - height_limit):
@@ -62,16 +64,20 @@ def do_morph(photo, fl, fl_type):
 photo_or = Image.open("Photos\\MyART.webp")
 
 fl = np.array([
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 0],
+    [0, 255, 0],
+    [0, 255, 0],
+    [0, 0, 0],
     ])
 
-fl_type = 'hit' # 'fit', 'hit'
+wild_point_loc = (1, 1)
+
+# print(fl[wild_point_loc])
+
+fl_type = 'fit' # 'fit', 'hit'
 ################################################
 photo = np.array(photo_or)
 
-tmp_ar = do_morph(photo, fl, fl_type)
+tmp_ar = do_morph(photo, fl, fl_type, wild_point_loc)
 
 photo = Image.fromarray(tmp_ar)
 ################################################
