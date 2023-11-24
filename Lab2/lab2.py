872df7f9e72b113197c9324d3353b9dc593e1949
Dadmon <maksklym-ipt27@lll.kpi.ua>
2023-11-24 14:20:45 +0200

    Works very fast
    Brand new redesign, progressbar added, loops for using files, ability to help find file type

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,8 +1,17 @@
 import numpy as np
 from PIL import Image
+
+from kernels import *
+
+from pprint import pprint
+from fnmatch import fnmatch
+# from tqdm import tqdm
+from numba import njit
+from numba_progress import ProgressBar
+
 import os
 import datetime as dtime
-from kernels import *
+
 
 def rgb_to_halftone(img):
     r, g, b = img[:,:,0], img[:,:,1], img[:,:,2]
@@ -11,145 +20,143 @@ def rgb_to_halftone(img):
 
     return halftone
 
-
-def do_multiplications(working_ar, ph_filter):
-    filtered_ar = np.zeros_like(working_ar)
-    # print(filtered_ar.dtype)
-    # print(working_ar.shape)
-    # print(ph_filter.shape)
-    if type(ph_filter) == np.ndarray:
-        
-        try:
-            height, width, channels = working_ar.shape
+@njit(nogil=True)
+def apply_filter(working_ar, ph_filter, filtered_ar, height, width, channels, height_limit, width_limit, progress):
+    for channel in range(channels):
+        for row in range(height_limit, height - height_limit):
             
-            height_fl, width_fl = ph_filter.shape
+            row_start = row - height_limit
+            row_finish = row + 2
             
-            height_limit = height_fl - 2
-            width_limit = width_fl - 2
-            
-            class Codes:
-                height_25 = height//4
-                height_50 = height//2
-                height_75 = (3*height)//4
-                height_100 = height - height_limit - 1
-                red = 0
-                green = 1
-                blue = 2
-            
-            for channel in range(channels):
-                
-                match channel:
-                    case Codes.red:
-                        print("Red layer started")
-                    case Codes.green:
-                        print("Green layer started")
-                    case Codes.blue:
-                        print("Blue layer started")
-                        
-                for row in range(height_limit, height - height_limit):
-                    
-                    match row:
-                        case Codes.height_25:
-                            print("25% Done")
-                        case Codes.height_50:
-                            print("50% Done")
-                        case Codes.height_75:
-                            print("75% Done")
-                        case Codes.height_100:
-                            print("100% Done")
-                    
-                    row_start = row - height_limit
-                    row_finish = row + 2
-                    
-                    for col in range(height_limit, width - height_limit):
-                        matrix = working_ar[row_start:row_finish, col - width_limit:col + 2, channel]
-                        npsum = np.sum(matrix * ph_filter)
-                        filtered_ar[row, col, channel] = npsum
-                
-                match channel:
-                    case Codes.red:
-                        print("Red layer done")
-                        print()
-                    case Codes.green:
-                        print("Green layer done")
-                        print()
-                    case Codes.blue:
-                        print("Blue layer done")
-                        print()
-                        
-        except ValueError:
-            height, width = working_ar.shape
-            channels = 1
-            
-            height_fl, width_fl = ph_filter.shape
-            
-            height_limit = height_fl - 2
-            width_limit = width_fl - 2
-            
-            class Codes:
-                height_25 = height//4
-                height_50 = height//2
-                height_75 = (3*height)//4
-                height_100 = height - height_limit - 1
-                red = 0
-                green = 1
-                blue = 2
-            
-            for channel in range(channels):
-                for row in range(height_limit, height - height_limit):
-                    
-                    match row:
-                        case Codes.height_25:
-                            print("25% Done")
-                        case Codes.height_50:
-                            print("50% Done")
-                        case Codes.height_75:
-                            print("75% Done")
-                        case Codes.height_100:
-                            print("100% Done")
-                    
-                    row_start = row - height_limit
-                    row_finish = row + 2
-                    
-                    for col in range(height_limit, width - height_limit):
-                        matrix = working_ar[row_start:row_finish, col - width_limit:col + 2]
-                        filtered_ar[row, col] = np.sum(matrix * ph_filter)
+            for col in range(width_limit, width - width_limit):
+                matrix = working_ar[row_start:row_finish, col - width_limit:col + 2, channel]
+                npsum = np.sum(matrix * ph_filter)
+                filtered_ar[row, col, channel] = npsum
+                progress.update(1)
     return filtered_ar
 
 
+# @njit(nogil=True)
+def do_multiplications(working_ar, ph_filter):
+    
+    filtered_ar = np.zeros_like(working_ar)
+    height, width, channels = working_ar.shape
+    
+    height_fl, width_fl = ph_filter.shape
+    
+    height_limit = height_fl - 2
+    width_limit = width_fl - 2
+    
+    with ProgressBar(total=(height - 2*height_limit)*(width - 2*width_limit)*channels) as progress:
+        return apply_filter(working_ar, ph_filter, filtered_ar, height, width, channels, height_limit, width_limit, progress)
+
+@njit(nogil=True)
 def scale(filtered_ar1):
-    print("Scaling array...")
     # maximum = np.max(filtered_ar1)
     minimum = np.min(filtered_ar1)
     filtered_ar1 = (255*(filtered_ar1 - minimum)/np.ptp(filtered_ar1)).astype(np.uint8)
     return filtered_ar1
+
     
-    
+@njit(nogil=True)  
 def clip(filtered_ar1):
-    print("Cliping array...")
     filtered_ar1 = np.clip(filtered_ar1, 0, 255).astype(np.uint8)
     return filtered_ar1
-    
+
+
+# @njit(nogil=True)
+def find_maybe_names(res_listdir, future_name):
+    examples = []
+    for filename in res_listdir:
+        if fnmatch(filename, future_name+'.*'):
+            examples += [filename]
+    return examples
+
+
+def filenamecheckget(res_listdir, to_continue):
+    filename = input('File Name: ')
+    maybe_names = find_maybe_names(res_listdir, filename)
+    if maybe_names == []:
+        return filenamecheckget(res_listdir, to_continue)
+    if filename == "":
+        return False
+    return filename, maybe_names
+
+
 ################################################
-photo_or = Image.open("Photos\\test.jpg")
-ph_filter = Inversia()
+kernels_dict = {
+    1:ZSUv(),
+    2:Inversia(),
+    3:Gauss_11x11(),
+    4:Move_diagonal(),
+    5:Rizkist(),
+    6:Sobel_diag(),
+    7:Border(),
+    8:My_Kernel()
+    }
+kernels_dict_name = {
+    1:'ZSUv',
+    2:'Inversia',
+    3:'Gauss 11x11',
+    4:'Moving diagonal',
+    5:'Sharpening',
+    6:'Sobel diagonal',
+    7:'Border',
+    8:'My Kernel'
+    }
 ################################################
-print()
-photo = np.array(photo_or, np.float64)
 try:
     os.mkdir('Results\\')
 except FileExistsError:
     pass
-################################################
-filtered_ar1 = do_multiplications(photo, ph_filter)
-# print(np.any(filtered_ar1 < -255), np.any(filtered_ar1 > 255))
-if np.sum(ph_filter) == 0 or (ph_filter.shape == Rizkist().shape and np.all(ph_filter  == Rizkist())):
-    filtered_ar1 = clip(filtered_ar1)
-else:
-    filtered_ar1 = scale(filtered_ar1)
-# print(np.any(filtered_ar1 < -255), np.any(filtered_ar1 > 255))
-photo = Image.fromarray(filtered_ar1.astype(np.uint8))
-################################################
-photo.show()
-photo.save('Results\\test_result_#' + str(dtime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")) + '.jpg')
-print('\nDone!')
+
+to_continue = True
+
+while to_continue:
+    
+    res_listdir = os.listdir('Photos\\')
+    
+    filename, maybe_names = filenamecheckget(res_listdir, to_continue)
+    if filename is False:
+        to_continue = False
+        continue
+    
+    print()
+    pprint(maybe_names)
+    print()
+    
+    filetype = input('File Type: ')
+    if filetype == "":
+        to_continue = False
+        continue
+    
+    print()
+    print('Codes for kernels: ')
+    pprint(kernels_dict_name)
+    print()
+    
+    kerneltype = input('Kernel: ')
+    if kerneltype == "":
+        to_continue = False
+        continue
+    
+    print()
+    print(kernels_dict_name[int(kerneltype)])
+    
+    photo_or = Image.open("Photos\\"+filename+"."+filetype)
+    ph_filter = kernels_dict[int(kerneltype)]
+    ################################################
+    photo = np.array(photo_or, np.float64)
+    filtered_ar1 = do_multiplications(photo, ph_filter)
+    print()
+    if np.sum(ph_filter) == 0 or (ph_filter.shape == Rizkist().shape and np.all(ph_filter  == Rizkist())):
+        print("Cliping array...")
+        filtered_ar1 = clip(filtered_ar1)
+    else:
+        print("Scaling array...")
+        filtered_ar1 = scale(filtered_ar1)
+    photo = Image.fromarray(filtered_ar1.astype(np.uint8))
+    ################################################
+    photo.show()
+    photo.save('Results\\test_result_#' + str(dtime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")) + '.jpg')
+    print('Done!\n')
